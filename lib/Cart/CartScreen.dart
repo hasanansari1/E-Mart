4b41bfda4bdb49a9d1ce8c5ffa0db4914969e131
hasanansari1 <hasanansarijulaya165@gmail.com>
2024-06-22 16:19:41 +0530

    first commit

--- lib/Cart/CartScreen.dart
+++ lib/Cart/CartScreen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_mart_user/color.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
@@ -21,7 +22,7 @@ class _AddToCartScreenState extends State<AddToCartScreen> {
   bool _isLoading = true;
 
   final NumberFormat _indianCurrencyFormat =
-  NumberFormat.currency(locale: 'en_IN', symbol: '₹');
+      NumberFormat.currency(locale: 'en_IN', symbol: '₹');
 
   String _formatCurrency(double amount) {
     return _indianCurrencyFormat.format(amount);
@@ -66,9 +67,12 @@ class _AddToCartScreenState extends State<AddToCartScreen> {
         _isLoading = false; // Set loading state to false when data is fetched
       });
     } catch (e) {
-      print('Error fetching products: $e');
+      if (kDebugMode) {
+        print('Error fetching products: $e');
+      }
       setState(() {
-        _isLoading = false; // Set loading state to false even if there's an error
+        _isLoading =
+            false; // Set loading state to false even if there's an error
       });
     }
   }
@@ -90,10 +94,11 @@ class _AddToCartScreenState extends State<AddToCartScreen> {
         ),
       );
     } catch (e) {
-      print('Error deleting product: $e');
+      if (kDebugMode) {
+        print('Error deleting product: $e');
+      }
     }
   }
-
 
   void _updateQuantity(ProductModel product, int quantity) async {
     try {
@@ -135,176 +140,178 @@ class _AddToCartScreenState extends State<AddToCartScreen> {
         backgroundColor: primaryColor,
         title: const Text(
           'Shopping Cart',
-          style: TextStyle(fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
         ),
         centerTitle: true,
         actions: _cartItems.isNotEmpty
             ? [
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              // Delete all products from the cart
-              _showDeleteAllConfirmationDialog();
-            },
-          ),
-        ]
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    // Delete all products from the cart
+                    _showDeleteAllConfirmationDialog();
+                  },
+                ),
+              ]
             : [],
       ),
       body: _isLoading
           ? const Center(
-          child: CircularProgressIndicator(),
-      )
+              child: CircularProgressIndicator(),
+            )
           : _cartItems.isEmpty
-          ? const Center(
-          child: Text(
-          'No items added to cart.',
-          style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),
-        ),
-      )
-          : Stack(
-          children: [
-            ListView.builder(
-            itemCount: _cartItems.length,
-            itemBuilder: (context, index) {
-              ProductModel product = _cartItems[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          ProductDetails(product: product)
+              ? const Center(
+                  child: Text(
+                    'No items added to cart.',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                )
+              : Stack(
+                  children: [
+                    ListView.builder(
+                      itemCount: _cartItems.length,
+                      itemBuilder: (context, index) {
+                        ProductModel product = _cartItems[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ProductDetails(product: product)),
+                            );
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListTile(
+                                contentPadding:
+                                    const EdgeInsets.all(0), // Added this line
+                                leading: Image.network(
+                                  product.images![0],
+                                  width: 100,
+                                  height: 100,
+                                ),
+                                title: Text(product.productName),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'M.R.P: ${product.newPrice}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Total Price: ${_formatCurrency(double.parse(product.totalprice))}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            IconButton(
+                                              icon: const Icon(Icons.remove),
+                                              onPressed: () {
+                                                int newQuantity = int.parse(
+                                                        product.selectedqty) -
+                                                    1;
+                                                if (newQuantity >= 0) {
+                                                  _updateQuantity(
+                                                      product, newQuantity);
+                                                }
+                                              },
+                                            ),
+                                            Text(
+                                                product.selectedqty.toString()),
+                                            IconButton(
+                                              icon: const Icon(Icons.add),
+                                              onPressed: () {
+                                                int newQuantity = int.parse(
+                                                        product.selectedqty) +
+                                                    1;
+                                                _updateQuantity(
+                                                    product, newQuantity);
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(Icons.delete),
+                                          onPressed: () {
+                                            _showDeleteConfirmationDialog(
+                                                product);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      contentPadding:
-                      const EdgeInsets.all(0), // Added this line
-                      leading: Image.network(
-                        product.images![0],
-                        width: 100,
-                        height: 100,
-                      ),
-                      title: Text(product.productName),
-                      subtitle: Column(
-                        crossAxisAlignment:
-                        CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'M.R.P: ${product.newPrice}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
+                  ],
+                ),
+      bottomNavigationBar: _cartItems.isNotEmpty
+          ? Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Subtotal: ${_formatCurrency(_subtotal)}',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_subtotal > 0) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                CheckOutScreen(cartItems: _cartItems),
                           ),
-                          Text(
-                            'Total Price: ${_formatCurrency(double.parse(product.totalprice))}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  IconButton(
-                                    icon: const Icon(Icons.remove),
-                                    onPressed: () {
-                                      int newQuantity = int.parse(
-                                          product.selectedqty) -
-                                          1;
-                                      if (newQuantity >= 0) {
-                                        _updateQuantity(
-                                            product, newQuantity);
-                                      }
-                                    },
-                                  ),
-                                  Text(product.selectedqty.toString()),
-                                  IconButton(
-                                    icon: const Icon(Icons.add),
-                                    onPressed: () {
-                                      int newQuantity = int.parse(
-                                          product.selectedqty) +
-                                          1;
-                                      _updateQuantity(
-                                          product, newQuantity);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.delete),
-                                onPressed: () {
-                                  _showDeleteConfirmationDialog(
-                                      product);
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
+                        );
+                      } else {
+                        Fluttertoast.showToast(
+                          msg: "Add items to the cart",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0,
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Theme.of(context).brightness == Brightness.dark
+                              ? Colors.white
+                              : Colors.black,
+                    ),
+                    child: Text(
+                      'Checkout',
+                      style: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.black
+                            : Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      bottomNavigationBar: _cartItems.isNotEmpty
-          ? Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Subtotal: ${_formatCurrency(_subtotal)}',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_subtotal > 0) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          CheckOutScreen(cartItems: _cartItems),
-                    ),
-                  );
-                } else {
-                  Fluttertoast.showToast(
-                    msg: "Add items to the cart",
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.CENTER,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0,
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).brightness ==
-                    Brightness.dark
-                    ? Colors.white : Colors.black,
+                ],
               ),
-              child: Text(
-                'Checkout',
-                style: TextStyle(
-                  color: Theme.of(context).brightness == Brightness.dark
-                      ? Colors.black : Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      )
+            )
           : null,
     );
   }
